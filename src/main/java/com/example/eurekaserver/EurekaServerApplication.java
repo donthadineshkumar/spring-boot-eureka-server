97c0888ea738a52e89076928f5c2c972b18d00ae
Dinesh Dontha <donthadineshkumar@gmail.com>
2020-09-08 12:57:03 +0530

    initial commit spring boot eureka server

---
+++ src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,23 @@
+package com.example.eurekaserver;
+
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
